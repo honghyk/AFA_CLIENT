f7a8edb4112485a4fa943c80325b2202bcfed812
honghyk <bhh970207@gmail.com>
2020-11-14 23:30:54 +0900

    implements binding adapter for fresco image uri

--- app/src/main/java/com/example/util/BindingAdapters.kt
+++ app/src/main/java/com/example/util/BindingAdapters.kt
@@ -1,7 +1,9 @@
 package com.example.util
 
+import android.net.Uri
 import android.view.View
 import androidx.databinding.BindingAdapter
+import com.facebook.drawee.view.SimpleDraweeView
 
 object BindingAdapters {
 
@@ -10,6 +12,16 @@ object BindingAdapters {
     fun setGone(v: View, gone: Boolean?) {
         if(gone != null) {
             v.visibility = if(gone) View.GONE else View.VISIBLE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("actualImageUri")
+    fun setActualImageUri(v: SimpleDraweeView, uri: String) {
+        if(uri.isNotEmpty()) {
+            v.setImageURI(uri, null)
+        } else {
+            v.setImageURI(null as Uri?, null)
         }
     }
 }
